591145e728ffb5e720a58a0f5b915e339adfc7bb
heav <hheav3@gmail.com>
2023-03-26 22:31:34 +0000

    made the game 34561x more optimal.

---
+++ game/sfx.lua
@@ -0,0 +1,21 @@
+local sounds = {
+    "harddrop",
+}
+
+local sources = {}
+for _, soundname in ipairs(sounds) do
+    sources[soundname] = love.audio.newSource("assets/"..soundname..".mp3","static")
+end
+
+local M = {}
+
+function M.play(name)
+    if sources[name] then
+        sources[name]:seek(0)
+        sources[name]:play()
+    else
+        error("no such sound effect: "..name)
+    end
+end
+
+return M
